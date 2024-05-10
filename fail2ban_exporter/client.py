3f5e964e51436204e84625d2ddc48da773427a7b
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-05-10 10:12:16 +0200

    Implement fail2ban client

---
+++ fail2ban_exporter/client.py
@@ -0,0 +1,88 @@
+import logging
+from typing import Optional, Self
+from fail2ban_exporter.protocol import F2BRequest, F2BResponse, F2BJail
+from fail2ban_exporter.socket import F2BSocket
+
+class F2BClient:
+    def __init__(self, host: str) -> Self:
+        self._host = host
+        self._socket = None
+        self.__open_socket()
+        self._logger = logging.getLogger()
+        
+    def __open_socket(self):
+        if self._socket:
+            try:
+                self._socket.close()
+            except Exception as e:
+                self._logger.error(f"Failed to gracefully close old socket: {e}")
+                
+        self._socket = F2BSocket(self._host)
+        
+    def __read(self) -> F2BResponse:
+        try:
+            return self._socket.read()
+        except Exception as e:
+            self._logger.warn(f"Failed to read data: {e}")
+            # try again
+            self.__open_socket()
+            return self.__read()
+    
+    def __write(self, data: F2BRequest):
+        try:
+            self._socket.write(data)
+        except Exception as e:
+            self._logger.warn(f"Failed to write data: {e}")
+            # try again
+            self.__open_socket()
+            self.__write(data)
+            
+    @staticmethod
+    def __assert_response_ok(response: F2BResponse):
+        if not response.is_success:
+            if isinstance(response.data, Exception):
+                raise response.data
+            elif isinstance(response.data, str):
+                raise RuntimeError(f"Fail2Ban server returned error: {response.data}")
+            else:
+                raise RuntimeError(f"Fail2Ban server returned status code {response.status_code}")
+            
+    def __write_read(self, data: F2BRequest) -> F2BResponse:
+        self.__write(data)
+        return self.__read()
+
+    def get_jail_names(self) -> list[str]:
+        response = self.__write_read(F2BRequest(["status"]))
+        F2BClient.__assert_response_ok(response)
+        jails = list(response.data[1][1:])
+        return jails
+
+    def get_jail_details(self, jail_name: str) -> F2BJail:
+        response = self.__write_read(F2BRequest(["status", jail_name]))
+        F2BClient.__assert_response_ok(response)
+        filter_data, action_data = response.data[0][1], response.data[1][1]
+        jail = F2BJail(
+            currently_failed=filter_data[0][1],
+            total_failed=filter_data[1][1],
+            filter_file_list=list(filter_data[2][1]),
+            currently_banned=action_data[0][1],
+            total_banned=action_data[1][1],
+            banned_ips=list(action_data[2][1])
+        )
+        
+        return jail
+    
+    def ban_ip(self, address: str, jail_name: str) -> bool:
+        response = self.__write_read(["set", jail_name, "banip", address])
+        F2BClient.__assert_response_ok(response)
+        return response.data[0] == 1
+    
+    def unban_ip(self, address: str, jail_name: Optional[str]) -> bool:
+        if jail_name is not None:
+            cmd = ["set", jail_name, "unbanip", address]
+        else:
+            cmd = ["unban", address]
+            
+        response = self.__write_read(cmd)
+        F2BClient.__assert_response_ok(response)
+        return response.data[0] == 1
--- fail2ban_exporter/protocol.py
+++ fail2ban_exporter/protocol.py
@@ -4,29 +4,50 @@ from typing import Any, Self
 PROTO_END_MSG = b"<F2B_END_COMMAND>"
 PROTO_CLOSE_MSG = b"<F2B_CLOSE_COMMAND>"
 
+
 class F2BMessage(ABC):
     def to_obj(self) -> list[Any]:
         pass
 
+
 class F2BRequest(F2BMessage):
     def __init__(self, command: list[Any]) -> Self:
         self.command = command
-        
+
     def to_obj(self) -> list[Any]:
         return self.command
-        
+
+
 class F2BResponse(F2BMessage):
     def __init__(self, status_code: int, data: Any) -> Self:
         self.status_code = status_code
         self.data = data
-        
+
     @property
     def is_success(self):
         return self.status_code == 0
-    
+
     @property
     def has_data(self):
         return True if self.data else False
-        
+
     def to_obj(self) -> list[Any]:
         return [self.status_code, *self.data]
+
+
+class F2BJail:
+    def __init__(
+        self,
+        currently_failed: int,
+        total_failed: int,
+        currently_banned: int,
+        total_banned: int,
+        filter_file_list: list[str],
+        banned_ips: list[str],
+    ) -> Self:
+        self.currently_failed = currently_failed
+        self.total_failed = total_failed
+        self.currently_banned = currently_banned
+        self.total_banned = total_banned
+        self.filter_file_list = filter_file_list
+        self.banned_ips = banned_ips
